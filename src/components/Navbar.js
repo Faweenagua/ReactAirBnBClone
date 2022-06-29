980d9f34472e3f1ba0593a3061cb44b50fe5f653
Francis Aweenagua <faweenagua@gmail.com>
2022-06-29 20:56:58 +0000

    completed nav bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AirbnbLogo from "../images/Airbnb_Logo.png";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <img className="logo" src={AirbnbLogo} alt="dssds" />
+    </nav>
+  );
+}
